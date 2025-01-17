5001f02b25ce8901e59bc93ebfc9666d84fbafe3
hungndhe150171 <hungndhe150171@fpt.edu.vn>
2025-01-18 03:37:16 +0700

    add something

--- SQL Script/Mustify SQL script.sql
+++ SQL Script/Mustify SQL script.sql
@@ -21,15 +21,25 @@ account_type int foreign key references User_type(type_id),
 is_active bit default 0,
 )
 
+create table Song_types(
+id int primary key identity,
+type_name nvarchar(255),
+description nvarchar(255),
+)
+
 create table Songs(
 song_id int primary key identity,
 title nvarchar(255) not null,
+type_id int foreign key references Song_types(id),
 artist nvarchar(255) not null,
 album nvarchar(255),
 duration int not null,
 file_path varchar(MAX) not null,
 created_date datetime default getdate(),
 )
+
+alter table Songs
+add image varchar(max)
 
 create table Playlists(
 playlist_id int primary key identity,
@@ -58,4 +68,14 @@ song_id int foreign key references Songs(song_id),
 played_at datetime default getdate(),
 )
 
+
+
+drop table User_type
+drop table Users
+drop table Song_types
+drop table Songs
+drop table Playlists
+drop table Playlist_songs
+drop table Likes
+drop table Recently_played
 
